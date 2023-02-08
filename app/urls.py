bcea57e7dd6b23bbaaf10552c781ff6250a80394
sarvarbeek3008 <118631515+djakbarovs@users.noreply.github.com>
2023-02-08 14:05:26 +0500

    ozgardi

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from app.views import Index, glasses, MaxPriceView
+from app.views import Index, glasses, index
 
 urlpatterns = [
-    # path('', Index.as_view(), name = 'index'),
-    path('', MaxPriceView.as_view(), name= 'index'),
+    path('', index, name = 'index'),
+    # path('', MaxPriceView.as_view(), name= 'index'),
     path('glasses/', glasses, name = 'glasses')
 ]
--- app/views.py
+++ app/views.py
@@ -9,20 +9,20 @@ class Index(View):
 # class Glasses(View):
 #     template_name = 'glasses.html'
 
-# def index(request):
-#     model = Glases.objects.order_by('-id')
-#     model2 =   Glases.objects.all().aggregate(Max('price'))
-#     context = {
-#         'glassess':model,
-#         'prices':model2
-#     }
-#     return render(request, 'index.html', context)
+def index(request):
+    model = Glases.objects.order_by('-id')
+    model2 =   Glases.objects.all().aggregate(Max('price'))
+    context = {
+        'glassess':model,
+        'prices':model2
+    }
+    return render(request, 'index.html', context)
 def glasses(request):
     return render(request, 'glasses.html')
 
-
-class MaxPriceView(View):
-    def get(self, request):
-        max_price = Glases.objects.all().aggregate(Max('price'))['price__max']
-        model = Glases.objects.order_by('-id')
-        return render(request, 'index.html', {'max_price': max_price}, {'glassess':model})
+#
+# class MaxPriceView(View):
+#     def get(self, request):
+#         # max_price = Glases.objects.all().aggregate(Max('price'))['price__max']
+#         model = Glases.objects.order_by('-id')
+#         return render(request, 'index.html', {'max_price': max_price}, {'glassess':model})
